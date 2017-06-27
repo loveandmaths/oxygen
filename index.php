0e6c6347046a04b320114d236e32bf0d6a3bc7b9
loveandmaths <alexandra.watson@hotmail.com>
2017-06-27 11:50:05 +1000

    Added changes to make work on iphone

--- index.php
+++ index.php
@@ -13,28 +13,27 @@
 	<link rel="stylesheet" type="text/css" href="assets/fonts/fonts.css" />
 	<link rel="stylesheet" type="text/css" href="assets/stylesheets/style.css" />
 	<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
-</head>
-<!-- body -->
-<body>
-	<!-- nav -->
-	<nav>
-		<header>
-			<h1>Oxygen</h1>
-		</header>
-		<div><a class="login" href="http://www.google.com" target="_blank">Login</a></div>
-	</nav>
-	<section class="coming-soon">
-		<p>Website coming soon</p>
-	</section>
-	<footer>
-		<p>
-			Oxygen Private Clients<br/>
-			Suite 2, Level 1, 74 Burwood Road, Hawthorn VIC 3122<br/>
-			<a href="mailto:info@oxygenpc.com.au">info@oxygenpc.com.au</a>
-		</p>
-	</footer>
-	<video autoplay loop poster="assets/images/background.png" id="video-background" muted plays-inline>
-		<source src="assets/video/background.mp4" type="video/mp4">
+	</head>
+	<!-- body -->
+	<body>
+		<!-- nav -->
+		<nav>
+			<header>
+				<h1>Oxygen</h1>
+			</header>
+			<div><a class="login" href="http://www.google.com" target="_blank">Login</a></div>
+		</nav>
+		<section class="coming-soon">
+			<p>Website coming soon</p>
+		</section>
+		<footer>
+			<p>
+				Oxygen Private Clients<br/>
+				Suite 2, Level 1, 74 Burwood Road, Hawthorn VIC 3122<br/>
+				<a href="mailto:info@oxygenpc.com.au">info@oxygenpc.com.au</a>
+			</p>
+		</footer>
+		<video loop autoplay controls="true" poster="assets/images/background.png" id="video-background" muted plays-inline src="assets/video/background.mp4" type="video/mp4">
 		</video>
 	</body>
 	</html>
