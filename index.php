67b88814709a6eedd385276ac5c10cc124f43eae
loveandmaths <alexandra.watson@hotmail.com>
2017-06-27 11:22:25 +1000

    Added .php in order to deploy to heroku

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype HTML>
+
+<!-- head -->
+
+<html lang="en">
+<head>
+	<title>OXYGEN</title>
+	<!-- meta -->
+	<meta charset=utf-8>
+	<meta name="description" content="Oxygen">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+	<!-- css -->
+	<link rel="stylesheet" type="text/css" href="assets/fonts/fonts.css" />
+	<link rel="stylesheet" type="text/css" href="assets/stylesheets/style.css" />
+</head>
+<!-- body -->
+<body>
+	<!-- nav -->
+	<nav>
+		<header>
+			<h1>Oxygen</h1>
+		</header>
+		<div><a class="login" href="www.google.com" target="_blank">Login</a></div>
+	</nav>
+	<section class="coming-soon">
+		<p>Website coming soon</p>
+	</section>
+	<footer>
+		<p>
+			Oxygen Private Clients<br/>
+			Suite 2, Level 1, 74 Burwood Road, Hawthorn VIC 3122<br/>
+			<a href="mailto:info@oxygenpc.com.au">info@oxygenpc.com.au</a>
+		</p>
+	</footer>
+	<video autoplay loop poster="assets/images/background.png" id="video-background" muted plays-inline>
+		<source src="assets/video/background.mp4" type="video/mp4">
+		</video>
+	</body>
+	</html>
